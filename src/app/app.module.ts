e756aeba87dcb4925a743e48141a3fe26148fc64
Matheus-Hespagnola <matheusah@ciandt.com>
2024-09-25 16:18:41 -0300

    config webapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { ChartModule } from 'primeng/chart';
 import { DockModule } from 'primeng/dock';
 import { ToolbarModule } from 'primeng/toolbar';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,13 @@ import { ToolbarModule } from 'primeng/toolbar';
     DividerModule,
     ChartModule,
     DockModule,
-    ToolbarModule
+    ToolbarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
